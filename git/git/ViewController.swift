e57973ccd995ab7e9ede7044fad0c36e2f2bd4b4
卢欢 <huan.lu@shanqing.com>
2018-03-22 16:38:35 +0800

    写了一个冒泡排序

--- git/git/ViewController.swift
+++ git/git/ViewController.swift
@@ -13,12 +13,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("git 成功!")
+      let a =  self.bubble_Sort(a: [3,5,2,1,6,4])
+        
+        print(a)
+     
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+    }
+    
+    func bubble_Sort(a: [Int])->Array<Int>{
+        var a = a
+        for i in 0 ... a.count-2 {
+            for k in 0 ... (a.count - 2 - i) {
+                var b = a[k]
+                if a[k] > a[k+1]{
+                    b = a[k+1]
+                    a[k+1] = a[k]
+                    a[k] = b
+                }
+            }
+        }
+        return a
     }
 
 
